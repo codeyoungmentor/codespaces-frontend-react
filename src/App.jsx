08ad1c65e4c7ac5810dc3741e0db18315f60775c
codeyoungmentor <160686339+codeyoungmentor@users.noreply.github.com>
2024-10-23 21:11:30 +0000

    basic-front-end-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [information , setInformation] = useState() ; 
+
+  const getInfo = async() => {
+    const response = await fetch("https://crispy-meme-7v7vg6595rrwcp5vq-3000.app.github.dev/")
+    const value = await response.text() ; 
+    setInformation(value); 
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <button onClick={getInfo}>Get information </button>
+      <h3>Information is : </h3>
+
+      <p>{information}</p>
     </div>
   );
 }
